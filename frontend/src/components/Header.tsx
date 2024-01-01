8e2f2950d7399d6aeb4cdffacde60b61c6706b6d
Kuwar20 <kuwarx1@gmail.com>
2024-01-01 19:25:49 +0530

    Refactor header and sign-in components

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -17,11 +17,11 @@ const Header = () => {
                 </span>
                 <span className='flex flex-col md:flex-row items-center space-x-2'>
                     {isLoggedIn ? <>
-                        <Link to="/my-bookings">My Bookings</Link>
-                        <Link to="/my-hotels">My Hotels</Link>
+                        <Link className='flex items-center text-white px-3 font-bold hover:bg-gray-400' to="/my-bookings">My Bookings</Link>
+                        <Link className='flex items-center text-white px-3 font-bold  hover:bg-gray-400' to="/my-hotels">My Hotels</Link>
                         <SignOutButton />
                     </> : <Link to="/sign-in"
-                        className='flex items-center bg-white text-blue-600 px-3 h-8 md:h-12 rounded-full font-bold hover:bg-gray-100'>
+                        className='flex items-center bg-white text-blue-600 px-3 h-8 md:h-12 rounded-full font-bold hover:bg-gray-600'>
                         Sign In
                     </Link>}
                 </span>
--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -2,7 +2,7 @@ import { useForm } from "react-hook-form";
 import { useMutation, useQueryClient } from "react-query";
 import * as apiClient from "../api-client";
 import { useAppContext } from "../contexts/AppContext";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export type SignInFormData = {
     email: string;
@@ -19,11 +19,11 @@ const SignIn = () => {
 
     const mutation = useMutation(apiClient.singIn, {
         onSuccess: async () => {
-            showToast({ message: "Sign in Successfull", type:"SUCCESS" });
+            showToast({ message: "Sign in Successfull", type: "SUCCESS" });
             await queryClient.invalidateQueries("validateToken");
             navigate("/");
         }, onError: (error: Error) => {
-            showToast({message: error.message, type:"ERROR"});
+            showToast({ message: error.message, type: "ERROR" });
         },
     });
 
@@ -62,7 +62,10 @@ const SignIn = () => {
                     <span className="text-red-500">{errors.password.message}</span>
                 )}
             </label>
-            <span>
+            <span className="flex items-center justify-between">
+                <span className="text-sm">
+                    Not Registered? <Link className="underline" to="/register">Create an account here</Link>
+                </span>
                 <button
                     type="submit"
                     className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl">Login</button>
