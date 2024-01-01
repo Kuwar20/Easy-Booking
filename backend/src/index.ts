5a85e6b8bdd918ab955e5443ef044da9794f7e9c
Kuwar20 <kuwarx1@gmail.com>
2024-01-01 20:03:09 +0530

    Update .gitignore and package.json
    Add cross-env package for environment variable configuration
    
    Update start script in package.json
    
    Add e2e script in package.json
    
    Update import statements in index.ts,
    
    Remove DB console from index.js (backend) for Prod

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,23 +1,29 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
+import express, { Request, Response } from "express";
+import cors from "cors";
 import "dotenv/config";
-import mongoose, { mongo } from 'mongoose';
-import userRoutes from './routes/user'
-import authRoutes from './routes/auth'
-import cookieParser from 'cookie-parser';
+import mongoose, { mongo } from "mongoose";
+import userRoutes from "./routes/user";
+import authRoutes from "./routes/auth";
+import cookieParser from "cookie-parser";
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+mongoose
+    .connect(process.env.MONGODB_CONNECTION_STRING as string)
+    .then(() =>
+        console.log("Connected to MongoDB :-", process.env.MONGODB_CONNECTION_STRING)
+    );
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-}));
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL,
+        credentials: true,
+    })
+);
 
-// this is a test endpoint/api route at '/api/test' to make sure everything is working properly  
+// this is a test endpoint/api route at '/api/test' to make sure everything is working properly
 // you can access using GET at http://localhost:7000/api/test
 // and it should return a json object with a message property containing "Hello from express endpoint" (ex: {"message":"Hello from express endpoint"})
 // app.get("/api/test", async (req: Request, res: Response) => {
@@ -26,12 +32,12 @@ app.use(cors({
 
 app.listen(7000, () => {
     console.log("Server is running on port 7000");
-})
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 // 1- Note :-
-// In a Node.js backend application, the index.js file serves as the 'entry point' of the application. 
+// In a Node.js backend application, the index.js file serves as the 'entry point' of the application.
 // It typically handles tasks such as Server Creation, Database Connection, Middleware Setup, Routes and Controllers, Models Initialization,
 // Environment Variables,Error Handling.
