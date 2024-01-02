c8352cf76518c26a42fdbeec6ed389e9a3b5b9d4
Kuwar20 <kuwarx1@gmail.com>
2024-01-02 14:34:39 +0530

    Add cloudinary package and configure cloudinary API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,13 @@ import userRoutes from "./routes/user";
 import authRoutes from "./routes/auth";
 import cookieParser from "cookie-parser";
 import path from "path";
+import {v2 as cloudinary} from "cloudinary";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY as string,
+    api_secret: process.env.CLOUDINARY_API_SECRET as string,
+});
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
