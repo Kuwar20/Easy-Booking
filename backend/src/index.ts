7842eb711216fc52eaf2629d58b16dc10f842c03
Kuwar20 <kuwarx1@gmail.com>
2024-01-04 22:13:22 +0530

    Add catch-all route for frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -44,6 +44,10 @@ app.use("/api/users", userRoutes);
 
 app.use("/api/my-hotels", myHotelRoutes);
 
+app.get("*", (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
+
 app.listen(7000, () => {
     console.log("Server is running on port 7000");
 });
