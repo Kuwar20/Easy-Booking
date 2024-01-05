8c507103a543b2868030ff4f9502866ce12e4895
Kuwar20 <kuwarx1@gmail.com>
2024-01-06 00:50:27 +0530

    Refactor hotel card key in MyHotels component

--- frontend/src/pages/MyHotels.tsx
+++ frontend/src/pages/MyHotels.tsx
@@ -30,7 +30,7 @@ const MyHotels = () => {
             </span>
             <div className="grid grid-cols-1 gap-8">
                 {hotelData.map((hotel) => (
-                    <div key={hotel.id}
+                    <div 
                         data-testid="hotel-card"
                         className="flex flex-col justify-between border border-slate-300 rounded-lg p-8 gap-5"
                     >
