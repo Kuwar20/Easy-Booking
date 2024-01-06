ae6d280a12022921ca33deb9ee43e7d3845911c9
Kuwar20 <kuwarx1@gmail.com>
2024-01-06 10:46:09 +0530

    Bug fix: Search Page filters overlaying on hotel results on smaller screens

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -68,9 +68,10 @@ const Search = () => {
         );
     };
 
-    return (
-        <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
-            <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
+return (
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+        <div className="lg:sticky lg:top-10">
+            <div className="rounded-lg border border-slate-300 p-5 h-fit">
                 <div className="space-y-5">
                     <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">
                         Filter by:
@@ -93,40 +94,40 @@ const Search = () => {
                     />
                 </div>
             </div>
-            <div className="flex flex-col gap-5">
-                <div className="flex justify-between items-center">
-                    <span className="text-xl font-bold">
-                        {hotelData?.pagination.total} Hotels found
-                        {search.destination ? ` in ${search.destination}` : ""}
-                    </span>
-                    <select
-                        value={sortOption}
-                        onChange={(event) => setSortOption(event.target.value)}
-                        className="p-2 border rounded-md"
-                    >
-                        <option value="">Sort By</option>
-                        <option value="starRating">Star Rating</option>
-                        <option value="pricePerNightAsc">
-                            Price Per Night (low to high)
-                        </option>
-                        <option value="pricePerNightDesc">
-                            Price Per Night (high to low)
-                        </option>
-                    </select>
-                </div>
-                {hotelData?.data.map((hotel) => (
-                    <SearchResultsCard hotel={hotel} />
-                ))}
-                <div>
-                    <Pagination
-                        page={hotelData?.pagination.page || 1}
-                        pages={hotelData?.pagination.pages || 1}
-                        onPageChange={(page) => setPage(page)}
-                    />
-                </div>
+        </div>
+        <div className="flex flex-col gap-5">
+            <div className="flex justify-between items-center">
+                <span className="text-xl font-bold">
+                    {hotelData?.pagination.total} Hotels found
+                    {search.destination ? ` in ${search.destination}` : ""}
+                </span>
+                <select
+                    value={sortOption}
+                    onChange={(event) => setSortOption(event.target.value)}
+                    className="p-2 border rounded-md"
+                >
+                    <option value="">Sort By</option>
+                    <option value="starRating">Star Rating</option>
+                    <option value="pricePerNightAsc">
+                        Price Per Night (low to high)
+                    </option>
+                    <option value="pricePerNightDesc">
+                        Price Per Night (high to low)
+                    </option>
+                </select>
+            </div>
+            {hotelData?.data.map((hotel) => (
+                <SearchResultsCard hotel={hotel} />
+            ))}
+            <div>
+                <Pagination
+                    page={hotelData?.pagination.page || 1}
+                    pages={hotelData?.pagination.pages || 1}
+                    onPageChange={(page) => setPage(page)}
+                />
             </div>
         </div>
-    );
-};
-
+    </div>
+);
+}
 export default Search;
