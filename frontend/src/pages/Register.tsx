7de3306211c7272eeb87ea8c6bb0775432c2fbbe
Kuwar20 <kuwarx1@gmail.com>
2024-07-12 00:42:43 +0530

    updated register password field logic and added show password logic

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -5,6 +5,7 @@ import { useAppContext } from "../contexts/AppContext";
 import { useNavigate } from "react-router-dom";
 import { JSXElementConstructor, ReactElement, ReactNode, ReactPortal, useState } from 'react';
 import Autosuggest from 'react-autosuggest';
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 export type RegisterFormData = {
     firstName: string;
@@ -21,7 +22,29 @@ const Register = () => {
     const { showToast } = useAppContext();
     const { register, watch, handleSubmit, formState: { errors }, setValue } = useForm<RegisterFormData>();
     const [email, setEmail] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
     const [suggestions, setSuggestions] = useState<string[]>([]);
+    const [passwordStrength, setPasswordStrength] = useState({
+        strength: 0,
+        label: "",
+    });
+    const [showPassword, setShowPassword] = useState(false);
+
+    const checkPasswordStrength = (password: any) => {
+        let strength = 0;
+        if (password.length >= 8) strength += 2;
+        if (password.length > 12) strength += 1;
+        if (password.match(/[a-z]+/)) strength += 1;
+        if (password.match(/[A-Z]+/)) strength += 1;
+        if (password.match(/[0-9]+/)) strength += 1;
+        if (password.match(/[$@#&!]+/)) strength += 1;
+
+        let strengthLabel = "Weak";
+        if (strength > 3) strengthLabel = "Moderate";
+        if (strength > 5) strengthLabel = "Strong";
+
+        return { strength, label: strengthLabel };
+    };
 
     const onEmailChange = (_event: any, { newValue }: any) => {
         setEmail(newValue);
@@ -44,12 +67,17 @@ const Register = () => {
     );
 
     const mutation = useMutation(apiClient.register, {
+        onMutate: () => {
+            setIsLoading(true);
+        },
         onSuccess: async () => {
+            setIsLoading(false);
             showToast({ message: "Registration successful", type: "SUCCESS" });
             await queryClient.invalidateQueries("validateToken");
             navigate("/");
         },
         onError: (error: Error) => {
+            setIsLoading(false);
             showToast({ message: error.message, type: "ERROR" });
         },
     });
@@ -121,47 +149,83 @@ const Register = () => {
                     <label className="text-grey-700 text-sm font-bold flex-1">
                         Password
                         <span className="text-red-500">*</span>
-                        <input
-                            type="password"
-                            className="mt-1 w-full border rounded-md px-3 py-2 placeholder-gray-500 text-gray-900 focus:z-10 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-900"
-                            {...register("password", {
-                                required: "Password is required",
-                                minLength: {
-                                    value: 8,
-                                    message: "Password must be at least 8 characters",
-                                },
-                                validate: (value) => {
-                                    const email = watch("email");
-                                    const hasUppercase = /[A-Z]/.test(value);
-                                    const hasLowercase = /[a-z]/.test(value);
-                                    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(value);
-                                    const isNotEqualEmail = value !== email;
-                                    const hasNumber = /\d/.test(value);
+                        <div className="relative">
+                            <input
+                                type={showPassword ? "text" : "password"}
+                                className="mt-1 w-full border rounded-md px-3 py-2 placeholder-gray-500 text-gray-900 focus:z-10 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-900"
+                                {...register("password", {
+                                    required: "Password is required",
+                                    minLength: {
+                                        value: 8,
+                                        message: "Password must be at least 8 characters",
+                                    },
+                                    validate: (value) => {
+                                        const email = watch("email");
+                                        const hasUppercase = /[A-Z]/.test(value);
+                                        const hasLowercase = /[a-z]/.test(value);
+                                        const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(value);
+                                        const isNotEqualEmail = value !== email;
+                                        const hasNumber = /\d/.test(value);
 
-                                    if (!hasUppercase) {
-                                        return "Password must contain at least 1 uppercase letter";
-                                    }
-                                    if (!hasLowercase) {
-                                        return "Password must contain at least 1 lowercase letter";
-                                    }
-                                    if (!hasSpecialChar) {
-                                        return "Password must contain at least 1 special character";
-                                    }
-                                    if (!isNotEqualEmail) {
-                                        return "Password must not be equal to email";
-                                    }
-                                    if (!hasNumber) {
-                                        return "Password must contain at least 1 number";
-                                    }
-                                    return true;
-                                },
-                            })}
-                        />
+                                        if (!hasUppercase) {
+                                            return "Password must contain at least 1 uppercase letter";
+                                        }
+                                        if (!hasLowercase) {
+                                            return "Password must contain at least 1 lowercase letter";
+                                        }
+                                        if (!hasSpecialChar) {
+                                            return "Password must contain at least 1 special character";
+                                        }
+                                        if (!isNotEqualEmail) {
+                                            return "Password must not be equal to email";
+                                        }
+                                        if (!hasNumber) {
+                                            return "Password must contain at least 1 number";
+                                        }
+                                        return true;
+                                    },
+                                })}
+                                onChange={(e) => {
+                                    setPasswordStrength(checkPasswordStrength(e.target.value));
+                                    register("password").onChange(e); // This ensures react-hook-form's onChange still runs
+                                }}
+                            />
+                            <div
+                                className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
+                                onClick={() => setShowPassword(!showPassword)}
+                            >
+                                {watch("password") && showPassword ? (
+                                    <FaEye className="h-5 w-5 text-gray-700" />
+                                ) : (
+                                    <FaEyeSlash className="h-5 w-5 text-gray-700" />
+                                )}
+                            </div>
+                        </div>
                         {errors.password && (
                             <span className="text-red-500">{errors.password.message}</span>
                         )}
                     </label>
-
+                    {watch("password") && (
+                        <div className="mt-1">
+                            <div className="w-1/2 bg-gray-200 rounded-full h-1">
+                                <div
+                                    className={`h-1 rounded-full ${passwordStrength.label === "Weak"
+                                        ? "bg-red-500"
+                                        : passwordStrength.label === "Moderate"
+                                            ? "bg-yellow-500"
+                                            : "bg-green-500"
+                                        }`}
+                                    style={{
+                                        width: `${(passwordStrength.strength / 7) * 100}%`,
+                                    }}
+                                ></div>
+                            </div>
+                            <div className="w-1/2 flex justify-between mt-1">
+                                <span className="text-xs text-gray-500">Weak</span>
+                                <span className="text-xs text-gray-500">Strong</span>
+                            </div>
+                        </div>
+                    )}
                     {/* Confirm Password */}
                     <label className="text-grey-700 text-sm font-bold flex-1">
                         Confirm Password
@@ -186,7 +250,14 @@ const Register = () => {
                     <span>
                         <button
                             type="submit"
-                            className="bg-green-500 hover:bg-green-700 text-white w-full p-2 rounded-md transition-colors duration-200">Create Account</button>
+                            disabled={isLoading}
+                            className={`w-full p-2 rounded-md transition-colors duration-200 ${isLoading
+                                ? 'bg-gray-400 cursor-not-allowed'
+                                : 'bg-green-500 hover:bg-green-700 text-white'
+                                }`}
+                        >
+                            {isLoading ? 'Loading...' : 'Create Account'}
+                        </button>
                     </span>
                 </form>
                 <div className="mt-6">
