344e1d3dc992c00467882e11aea95b9d0f733482
Kuwar20 <kuwarx1@gmail.com>
2024-07-12 02:44:24 +0530

    Major fix: width is set to be all screen for all device

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const Layout = ({ children }: Props) => {
     return (
-        <div className='flex flex-col min-h-screen'>
+        <div className='flex flex-col min-h-screen w-full overflow-x-hidden'>
             <Header />
             <Hero />
             <div className='container mx-auto'>
