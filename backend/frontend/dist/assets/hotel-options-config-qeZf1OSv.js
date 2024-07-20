dee7a1c63c067e45623fd9da30a8a4e00ba63b92
Kuwar20 <kuwarx1@gmail.com>
2024-07-20 16:59:55 +0530

    finding out

---
+++ backend/frontend/dist/assets/hotel-options-config-qeZf1OSv.js
@@ -0,0 +1,1 @@
+const e=["Budget","Boutique","Luxury","Ski Resort","Business","Family","Romantic","Hiking Resort","Cabin","Beach Resort","Golf Resort","Motel","All Inclusive","Pet Friendly","Self Catering"],o=["Free WiFi","Parking","Airport Shuttle","Family Rooms","Non-Smoking Rooms","Outdoor Pool","Spa","Fitness Center"];export{o as a,e as h};
