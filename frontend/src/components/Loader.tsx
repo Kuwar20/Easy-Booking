07b595d17f4861e6c84efa1a3cff34f6b7e51ad4
Kuwar20 <kuwarx1@gmail.com>
2024-09-06 20:10:33 +0530

    Update loader component using tailwind css for better loading and reducing resource.
    - Remove unused loading-twotone-loop.svg file
    - Update Loader component to use a CSS-based spinner instead of an SVG image

--- frontend/src/components/Loader.tsx
+++ frontend/src/components/Loader.tsx
@@ -1,11 +1,11 @@
-import LoadingIcon from '../assets/loading-twotone-loop.svg';
-
 const Loader = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-white dark:bg-gray-800 transition-colors duration-300 text-gray-900 dark:text-black">
-      <img src={LoadingIcon} alt="SVG Image"/>
+      <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-blue-500"></div>
+      </div>
     </div>
   );
-}
+};
 
 export default Loader;
